988c04c69546e065ffb278fe91ae2bf81f6c5b46
Abdelmoez Ashraf <92996750+abmoez@users.noreply.github.com>
2023-08-30 21:36:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { Socket } = require("dgram");
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 12345;
 
 const server = app.listen(PORT, () => {
   console.log(`Listenning on port ${PORT}`);
